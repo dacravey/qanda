f494c19344e81dabb18f73007e0ac1cabb335867
Debbie Cravey <cinemaestro@yahoo.com>
2015-03-24 06:52:00 -0700

    update question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Question do
-  it {should validate_presence_of :inquiry }
+  it { should validate_presence_of :inquiry }
   it { should validate_presence_of :tag }
-  it {should have_many :responses }
+  it { should have_many :responses }
 end
